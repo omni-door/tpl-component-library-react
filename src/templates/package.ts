b081d4181ea9c04143fd798cf898dc91866aa45c
bobby.li <bobby.lhan@gmail.com>
2020-02-22 16:00:41 +0800

    🐸  [OMNI-DOOR/TPL-COMPONENT-LIBRARY-REACT]: 0.0.11

--- src/templates/package.ts
+++ src/templates/package.ts
@@ -117,7 +117,7 @@ export default (config: {
   ${
     type_react && ts && strategy === 'stable'
       ? `"resolutions": {
-        "@types/react": '${type_react}'
+        "@types/react": "${type_react}"
       },`
       : ''
   }
